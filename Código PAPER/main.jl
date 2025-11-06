5e53f5b3387339ad5de95acc55ee35099b66b812
Felipe Cid <127272880+FelipeUCid@users.noreply.github.com>
2025-11-05 22:40:52 -0300

    Delete Código PAPER directory

--- Código PAPER/main.jl
+++
@@ -1,8 +0,0 @@
-include("modelo/unit_commitment.jl")
-include("modelo/parametros.jl")
-include("modelo/variables.jl")
-include("modelo/restricciones.jl")
-
-using .ModeloUC
-modelo, par, set, var = ModeloUC.construir_modelo()
-status = ModeloUC.solve_modelo(modelo)
